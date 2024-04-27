cba03d75f48619eae79ecfe29f16719cc2aec676
adnanbot1 <102922097+adnanbot1@users.noreply.github.com>
2024-04-27 14:11:07 +0300

    Update config.js

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ module.exports = {
   ANTILINK: toBool(process.env.ANTI_LINK) || false,
   LOGS: toBool(process.env.LOGS) || true,
   ANTILINK_ACTION: process.env.ANTI_LINK || "kick",
-  SESSION_ID:process.env.SESSION_ID || " ",
+  SESSION_ID:process.env.SESSION_ID || "IZUMI-MD-666~VBGBYLMpY7DKCx1kWVA1A",
   LANG: process.env.LANG || "EN",
   HANDLERS: process.env.PREFIX || '^[.]',
   BRANCH: "main",
